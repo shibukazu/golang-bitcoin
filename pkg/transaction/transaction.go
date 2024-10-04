98fd1c8dd29b04e149fe00bbc63cb0fd1c32ca40
shibukazu <kazuki.shibutani.private@gmail.com>
2024-10-04 19:03:49 +0900

    impl tx fetcher

--- pkg/transaction/transaction.go
+++ pkg/transaction/transaction.go
@@ -1,8 +1,12 @@
 package transaction
 
 import (
+	"crypto/sha256"
 	"encoding/binary"
+	"encoding/hex"
+	"fmt"
 	"io"
+	"net/http"
 )
 
 type Transaction struct {
@@ -92,8 +96,19 @@ func (t *Transaction) Serialize() ([]byte, error) {
 		serialized = append(serialized, output.Serialize()...)
 	}
 	binary.LittleEndian.PutUint32(serialized, t.Locktime)
-	
+
 	return serialized, nil
+}
+
+func (t *Transaction) ID() (string, error) {
+	serialized, err := t.Serialize()
+	if err != nil {
+		return "", err
+	}
+	firstHash := sha256.Sum256(serialized)
+	secondHash := sha256.Sum256(firstHash[:])
+
+	return hex.EncodeToString(secondHash[:]), nil
 }
 
 func ParseInput(reader io.Reader) (*Input, error) {
@@ -131,6 +146,24 @@ func (i *Input) Serialize() []byte {
 	serialized = append(serialized, i.ScriptSig.Serialize()...)
 	binary.LittleEndian.PutUint32(serialized, i.Sequence)
 	return serialized
+}
+
+func (i *Input) Value(testnet bool) (uint64, error) {
+	fetcher := NewTransactionFetcher(testnet)
+	tx, err := fetcher.FetchTransaction(hex.EncodeToString(i.PreviousOutputHash), false)
+	if err != nil {
+		return 0, err
+	}
+	return tx.Outputs[i.PreviousOutputIndex].Value, nil
+}
+
+func (i *Input) ScriptPubKey(testnet bool) (*ScriptPubKey, error) {
+	fetcher := NewTransactionFetcher(testnet)
+	tx, err := fetcher.FetchTransaction(hex.EncodeToString(i.PreviousOutputHash), false)
+	if err != nil {
+		return nil, err
+	}
+	return tx.Outputs[i.PreviousOutputIndex].ScriptPubKey, nil
 }
 
 func ParseScriptSig(reader io.Reader) (*ScriptSig, error) {
@@ -176,4 +209,52 @@ func ParseScriptPubKey(reader io.Reader) (*ScriptPubKey, error) {
 
 func (s *ScriptPubKey) Serialize() []byte {
 	return nil
+}
+
+type TransactionFetcher struct {
+	url    string
+	cached map[string]*Transaction
+}
+
+func NewTransactionFetcher(testnet bool) *TransactionFetcher {
+	var url string
+	if testnet {
+		url = "http://testnet.programmingbitcoin.com"
+	} else {
+		url = "http://mainnet.programmingbitcoin.com"
+	}
+	return &TransactionFetcher{url, make(map[string]*Transaction)}
+}
+
+func (tf *TransactionFetcher) FetchTransaction(txid string, fresh bool) (*Transaction, error) {
+	if !fresh && tf.cached[txid] != nil {
+		return tf.cached[txid], nil
+	}
+	url := fmt.Sprintf("%s/tx/%s.hex", tf.url, txid)
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("error fetching transaction: %s", resp.Status)
+	}
+
+	tx, err := ParseTransaction(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	actualTxid, err := tx.ID()
+	if err != nil {
+		return nil, err
+	}
+	if actualTxid != txid {
+		return nil, fmt.Errorf("fetched transaction id does not match expected: %s != %s", actualTxid, txid)
+	}
+
+	tf.cached[txid] = tx
+
+	return tx, nil
 }
